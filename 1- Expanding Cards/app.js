350eeeaed07f09335793adbce7863358d0f87a1a
Marco-Silvera <marksilver_1_0@hotmail.com>
2023-11-15 00:59:07 -0500

    Comentando paso a paso el código

--- 1- Expanding Cards/app.js
+++ 1- Expanding Cards/app.js
@@ -1,11 +1,17 @@
+// Seleccionamos todos los elementos de clase "panel"
 const panels = document.querySelectorAll(".panel");
 
+// Iteramos entre los elementos "panel" con forEach para buscar el evento "click"
 panels.forEach((panel) => {
     panel.addEventListener("click", () => {
+        // Elimina los "active" de todos los paneles
         remoteActiveClasses();
+        // Agrega "active" al panel clickeado
         panel.classList.add("active");
     });
 });
+
+// Esta funcion elimina el "active" de todos los paneles
 const remoteActiveClasses = () => {
     panels.forEach((panel) => {
         panel.classList.remove("active");
